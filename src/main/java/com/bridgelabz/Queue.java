540e3ab7eada4c41fac3c6ea920c75a39f3e9a7c
saranyellanki <saran.yellanki12@gmail.com>
2021-11-07 13:25:49 +0530

    Add enqueue method to append data

--- src/main/java/com/bridgelabz/Queue.java
+++ src/main/java/com/bridgelabz/Queue.java
@@ -16,7 +16,7 @@ public class Queue<T> {
      * @param data is a T type data which is added to list
      */
     public void enqueue(T data){
-        linkedList.add(data);
+        linkedList.append(data);
     }
 
     public static void main(String[] args) {
